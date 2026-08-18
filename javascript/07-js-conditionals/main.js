35cda3055fafe29b9ad5be99da97c12c89a2f5bc
marcusdev <marcus21.dev@gmail.com>
2026-08-17 21:25:49 -0300

    docs: add conditionals examples

---
+++ javascript/07-js-conditionals/main.js
@@ -0,0 +1,46 @@
+const sum = 1 + 2;
+
+if (sum === 2) {
+  console.log("Sum is 2!");
+} else if (sum === 3) {
+  console.log("Sum is 3!");
+} else {
+  console.log("Sum is not 2!");
+}
+
+// &&(and) - ||(or)
+
+const sum1 = 2 + 2;
+const sum2 = 3 + 4;
+
+if (sum1 === 4 && sum2 === 6) {
+  console.log("Sum is 4 and Sum2 is 6!");
+}
+
+if (sum1 === 4 || sum2 === 6) {
+  console.log("Sum is 4 and Sum2 is 6!");
+}
+
+let number = sum === 2 ? 2 : 4;
+
+// if (sum === 2) {
+number = 2;
+// } else {number = 4;}
+
+console.log(number);
+
+// ======================================================================
+
+const car = "Mercedes";
+
+switch (car) {
+  case "Mercedes":
+    console.log("Mercedes is beautiful!");
+    break;
+  case "Ferrari":
+    console.log("Ferrari is very red!");
+    break;
+  case "Tesla":
+    console.log("Tesla is very smart!");
+    break;
+}
